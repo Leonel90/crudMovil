e8b5798c24ddbd4e35ac960969603ae76697feff
unknown <sleonel1990@gmail.com>
2023-12-04 09:02:35 -0500

    Script para crear la tabla en DB

--- lib/src/db/operation.dart
+++ lib/src/db/operation.dart
@@ -3,6 +3,10 @@ import 'package:sqflite/sqflite.dart';
 
 class Operation {
   Future<Database> _openDB() {
-    return openDatabase(join(getDatabasesPath() as String, "note.db"));
+    return openDatabase(join(getDatabasesPath() as String, "note.db"),
+        onCreate: (db, version) {
+      return db.execute(
+          "CREATE TABLE notes (id INTEGER PRIMARY KEY, title TEXT, content TEXT)");
+    }, version: 1);
   }
 }
